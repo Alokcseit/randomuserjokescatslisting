ae8805a98ab2fd9d75498abc6810fee80fe55b37
Alokcsit <swainalokkumar25@gmail.com>
2024-08-14 07:17:13 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import { HashRouter as Router , Routes, Route } from "react-router-dom";
 import CatsListing from "./CatsListing";
 import RandomJokes from "./RandomJokes";
 import RandomUser from "./RandomUser";
