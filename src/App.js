7b5ee7b4084adea2e6cc62c245255e47a0cd71ef
Alokcsit <swainalokkumar25@gmail.com>
2024-08-14 08:11:32 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
-import { HashRouter as Router , Routes, Route } from "react-router-dom";
+import {  Routes, Route } from "react-router-dom";
 import CatsListing from "./CatsListing";
 import RandomJokes from "./RandomJokes";
 import RandomUser from "./RandomUser";
 import Home from "./Home";
 function App() {
   return (
-  <Router>
+  
    <Routes>
      <Route path="/" element={<Home />} />
      <Route path="/users" element={<RandomUser />} />
      <Route path="/jokes" element={<RandomJokes/>} />
      <Route path="/cats" element={<CatsListing/>} />
    </Routes>
-  </Router>
+ 
   );
 }
 
